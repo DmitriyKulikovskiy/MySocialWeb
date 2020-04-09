88db55338125bddce3114db2bb20721b0be08ab2
DmitriyKulikovskiy <59229226+DmitriyKulikovskiy@users.noreply.github.com>
2020-04-09 22:44:58 +0300

    Updates

---
+++ precache-manifest.081e8bfd9e8c5e756e5b245dd68571ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921c76596a1909854eb922fd58e49e3d",
+    "url": "/MySocialWeb/index.html"
+  },
+  {
+    "revision": "0cb9ac9aaab9b2b642bd",
+    "url": "/MySocialWeb/static/css/3.c9f65675.chunk.css"
+  },
+  {
+    "revision": "ebe76f37b92f21c358da",
+    "url": "/MySocialWeb/static/css/4.c93af2f7.chunk.css"
+  },
+  {
+    "revision": "2dae5f73008e70b53f82",
+    "url": "/MySocialWeb/static/css/main.04591c68.chunk.css"
+  },
+  {
+    "revision": "9d00afed41bc6eda99f7",
+    "url": "/MySocialWeb/static/js/2.abf7426f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/MySocialWeb/static/js/2.abf7426f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb9ac9aaab9b2b642bd",
+    "url": "/MySocialWeb/static/js/3.4c32940f.chunk.js"
+  },
+  {
+    "revision": "ebe76f37b92f21c358da",
+    "url": "/MySocialWeb/static/js/4.761450fb.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/MySocialWeb/static/js/4.761450fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dae5f73008e70b53f82",
+    "url": "/MySocialWeb/static/js/main.5d32b41f.chunk.js"
+  },
+  {
+    "revision": "6d9f7990979674ff610d",
+    "url": "/MySocialWeb/static/js/runtime-main.d812973c.js"
+  }
+]);
